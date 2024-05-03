4bbd2bbc5def0981370a4e99ba371c4febf5eda4
ramshukla88 <ram007maan@gmail.com>
2024-05-04 03:34:37 +0530

    Create Intro_6_leap_year.py
    completed

---
+++ Intro_6_leap_year.py
@@ -0,0 +1,15 @@
+# Date: 04-May-2024
+# Author: Ram Shukla
+# Location: Hyderabad, India
+
+def is_leap(year):
+    leap = False
+    if year % 4 ==  0:
+        leap = True
+        if year % 100 == 0 and year % 400 !=0:
+            leap = False
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
